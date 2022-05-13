7d4301ffff78fb0ee375b7cf2fc290db02b0e2f1
Danijel Beljan <danijel.beljan@protonmail.com>
2022-05-13 14:16:48 +0200

    added docs

--- rollup.config.js
+++ rollup.config.js
@@ -17,8 +17,10 @@ function getHtmlOptions() {
             return getHtml({
                 // These 3 options are the most important. Without them the static website will not function.
                 layout: LAYOUT, // Type of layout.
-                builtCssFilename, // Name of built js file. Will be appended at the end of the body tag.
-                builtJsFilename: options.files.js[0].fileName, // Name of the built css file. Will be the last included css file in the head.
+                builtCssFilename, // Will be appended at the end of <head>.
+                builtJsFilename: options.files.js[0].fileName, // Will appended to the end of <body>.
+
+                // Create mock elements to simulate your article.
                 content: `
                     ${createSubtitle('Test Subtitle')}
                     ${createParagraph('Test paragraph')}
@@ -31,10 +33,13 @@ function getHtmlOptions() {
                 lead: 'Test lead.',
                 title: 'Test title',
 
+                // Links to other css files that need to be loaded.
+                // Will be appended to <head> but before the bundled css file of your app.
                 customCssLinks: [
                     'https://service.sophie.nzz.ch/bundle/sophie-q@^1,sophie-input@^1,sophie-font@^1,sophie-color@^1,sophie-viz-color@^1,sophie-legend@^1.css',
                 ],
 
+                // Links to other js filed that need to be loaded.
                 // Will be appended at the end of the body, but before the built js file for the app.
                 customJsLinks: [
                     'https://cdn.polyfill.io/v2/polyfill.min.js?features=Map,URL,Promise,fetch,URLSearchParams,Array.prototype.find,Array.prototype.findIndex,Object.entries,Array.prototype.includes,CustomEvent,Array.from,String.prototype.startsWith&flags=gated&unknown=polyfill'
